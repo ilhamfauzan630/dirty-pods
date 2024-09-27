38f6b42c782ae5adbb1f04fd21a2fa1ea33bd028
Ilham Fauzan <fauzan@LAPTOP-T6DE5A9I>
2024-09-27 21:34:36 +0700

    fix ui landing page

--- src/dirty_frontend/src/components/LandingPage.js
+++ src/dirty_frontend/src/components/LandingPage.js
@@ -1,6 +1,15 @@
 import { html, render } from 'lit-html';
 import './styles/landing.css';
-import { navigate } from './App';  // Import fungsi navigate
+import imgvote from './../../assets/mainpict.png'
+import shield from './../../assets/shield.png'
+import glasseye from './../../assets/glasseye.png'
+import userfriendly from './../../assets/userfriendly.png'
+import snowlylulu from './../../assets/snowlylulu.png'
+import fb from './../../assets/logo_facebook.png'
+import twitter from './../../assets/logo_twitter.png'
+import instagram from './../../assets/logo_instagram.png'
+import linkedIn from './../../assets/logo_linkedin.png'
+
 
 export const renderLandingPage = (navigate) => {
   let body = html`
@@ -24,7 +33,7 @@ export const renderLandingPage = (navigate) => {
       <!-- Hero Section -->
       <section class="hero">
         <div class="hero-content">
-          <img src="../../assets/mainpict.png" alt="Voting Image" class="hero-image">
+          <img src="${imgvote}" alt="Voting Image" class="hero-image">
           <div class="hero-text">
             <h1>Welcome to Blockchain Voting App</h1>
             <p>Join the voting revolution using blockchain technology. Easy, safe and transparent.</p>
@@ -41,17 +50,17 @@ export const renderLandingPage = (navigate) => {
         <h2>Features</h2>
         <div class="feature-grid">
           <div class="feature-item">
-            <img src="../../assets/shield.png" alt="Guaranteed Transparency" class="hero-image2">
+            <img src="${glasseye}" alt="Guaranteed Transparency" class="hero-image2">
             <h3>Guaranteed Transparency</h3>
             <p>Every vote cast is transparently recorded on the blockchain, ensuring tamper-proof results.</p>
           </div>
           <div class="feature-item">
-            <img src="../../assets/glasseye.png" alt="High-Level Security">
+            <img src="${shield}" alt="High-Level Security">
             <h3>High-Level Security</h3>
             <p>Our blockchain technology ensures that voting data and results are protected from security threats.</p>
           </div>
           <div class="feature-item">
-            <img src="../../assets/userfriendly.png" alt="User-Friendly">
+            <img src="${userfriendly}" alt="User-Friendly">
             <h3>User-Friendly</h3>
             <p>The user-friendly interface simplifies the voting process and result monitoring.</p>
           </div>
@@ -80,15 +89,15 @@ export const renderLandingPage = (navigate) => {
       <!-- Footer -->
       <footer>
         <div class="logoteam">
-          <a href="#"><img src="../../assets/snowlylulu.png" alt="teamimg"></a>
+          <a href="#"><img src="${snowlylulu}" alt="teamimg"></a>
         </div>  
         <div class="social-media">
           <p>Find Us In Social Media</p> 
           <div class="socmed-img">
-          <a href="#"><img src="../../assets/logo_instagram.png" alt="Instagram"></a> 
-          <a href="#"><img src="../../assets/logo_facebook.png" alt="Facebook"></a>
-          <a href="#"><img src="../../assets/logo_linkedin.png" alt="LinkedIn"></a>
-          <a href="#"><img src="../../assets/logo_twitter.png" alt="Twitter"></a>
+          <a href="#"><img src="${instagram}" alt="Instagram"></a> 
+          <a href="#"><img src="${fb}" alt="Facebook"></a>
+          <a href="#"><img src="${linkedIn}" alt="LinkedIn"></a>
+          <a href="#"><img src="${twitter}" alt="Twitter"></a>
           </div>
         </div>
         <div class="footer-links">
